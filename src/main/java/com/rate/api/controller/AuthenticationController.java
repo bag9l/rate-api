793657a12935528d229e5f345f3a8d4125a6aacf
bag9l <vadimlyashok03@gmail.com>
2023-06-03 22:07:15 +0300

    try login-success route

--- src/main/java/com/rate/api/controller/AuthenticationController.java
+++ src/main/java/com/rate/api/controller/AuthenticationController.java
@@ -2,22 +2,18 @@ package com.rate.api.controller;
 
 import com.rate.api.dto.AuthenticationRequest;
 import com.rate.api.dto.AuthenticationResponse;
-import com.rate.api.model.Role;
 import com.rate.api.model.User;
 import com.rate.api.service.AuthenticationService;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import java.util.Collection;
 
 @RequiredArgsConstructor
 @RestController
@@ -35,21 +31,30 @@ public class AuthenticationController {
         );
     }
 
+//    @PostMapping("/login-success")
+//    public ResponseEntity<User> getLoginInfo(@AuthenticationPrincipal User user,
+//                                             HttpServletResponse response) throws IOException {
+//        System.out.println("*********************************");
+//        System.out.println("SUCCESS");
+//        System.out.println("*********************************");
+//        if (user.getAuthorities()
+//                .contains(new SimpleGrantedAuthority(Role.LECTURER.name())) || user.getAuthorities()
+//                .contains(new SimpleGrantedAuthority(Role.STUDENT.name()))) {
+//            response.sendRedirect("/subjects");
+//        } else if (user.getAuthorities()
+//                .contains(new SimpleGrantedAuthority(Role.ADMIN.name()))) {
+//            response.sendRedirect("/admins/departments");
+//        }
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(user);
+//    }
+
     @PostMapping("/login-success")
-    public ResponseEntity<User> getLoginInfo(@AuthenticationPrincipal User user,
-                                             HttpServletResponse response) throws IOException {
+    public ResponseEntity<String> getRole(@AuthenticationPrincipal User user) {
         System.out.println("*********************************");
         System.out.println("SUCCESS");
         System.out.println("*********************************");
-        if (user.getAuthorities()
-                .contains(new SimpleGrantedAuthority(Role.LECTURER.name())) || user.getAuthorities()
-                .contains(new SimpleGrantedAuthority(Role.STUDENT.name()))) {
-            response.sendRedirect("/subjects");
-        } else if (user.getAuthorities()
-                .contains(new SimpleGrantedAuthority(Role.ADMIN.name()))) {
-            response.sendRedirect("/admins/departments");
-        }
 
-        return ResponseEntity.status(HttpStatus.OK).body(user);
+        return ResponseEntity.status(HttpStatus.OK).body(user.getRole().getValue());
     }
 }
